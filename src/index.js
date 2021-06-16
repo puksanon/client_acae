22c2afa369d00ed1ba83cac8227f4d737e81f32a
MuchaluchaZ <kittipit22@hotmail.com>
2021-06-16 22:29:55 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const zlib = require("zlib");
 const fs = require("fs");
 const base64converter = require("./base64_converter");
-const tf = require("@tensorflow/tfjs-node");
-
+// const tf = require("@tensorflow/tfjs-node");
+const tf = require("@tensorflow/tfjs");
 const inference = async (latent_vector) => {
   try {
-    const model = await tf.loadLayersModel("file://node_modules/@dcae_client/dcae_decoder/src/decoder_model/model.json");
+    const model = await tf.loadLayersModel("https://raw.githubusercontent.com/Untesler/DCAE_Compressor/main/decoder_model/model.json");
     let tensor  =  tf.tensor(latent_vector).reshape([1, 2048]);
     let decoded = model.predict(tensor);
     decoded     =  decoded.mul(255).reshape([128, 128, 3]);
